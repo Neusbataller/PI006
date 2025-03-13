d029e7ba92aaa99ed0fe1ef76d9a1675b7ad3e61
Neusbataller <nebaib@alumnatflorida.es>
2025-03-13 18:06:42 +0100

    cambios

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.set("port", process.env.PORT || 8080);
 //Middleware
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.json());
 
 //Rutas
-app.use("/", require("./routes/index"));
+app.use(require("./routes/index"));
 
 //Iniciando el servidor
 app.listen(app.get("port"), () => {
--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,41 @@ const { Router } = require("express");
 const router = Router();
 
 //Rutas Get
-router.get("/rutaGet", (req, res) => {
+router.get("/", (req, res) => {
   console.log("Alguien ha accedido al servidor ");
   res.send("Bienvenido al servidor ");
 });
 
+router.get("/rutaGet", (req, res) => {
+  console.log("Alguien ha accedido a la ruta get ");
+  res.send("hola haciendo pruebas con la ruta get ");
+});
+router.get("/rutaGetJSON", (req, res) => {
+  console.log("Alguien ha accedido a la ruta get con un JSON ");
+  res.send("hola haciendo pruebas con la ruta getJSON ");
+});
+
 router.post("/rutaPOST", (req, res) => {
-  console.log("Enviada una peticion POST");
+  console.log("Alguien ha accededido a la ruta POST");
+  res.send("hola haciendo pruebas POST");
+});
+
+router.post("/rutaPOSTconBody", (req, res) => {
   console.log(req.body);
-
-  res.json({
-    id: "cliente_01",
-    nombre: req.body.nombre,
-    apellido1: req.body.apellido1,
-    apellido2: req.body.apellido2,
-    edad: req.body.edad,
-    correo: req.body.correo,
-    telefono: req.body.telf,
-  });
-
-  // res.send("Gracias por rellenar el formulario :) ");
-  // res.redirect("http://127.0.0.1:5500/index.html");
+  res.send("Info recibida");
 });
 
 module.exports = router;
+
+//   res.json({
+//     id: "cliente_01",
+//     nombre: req.body.nombre,
+//     apellido1: req.body.apellido1,
+//     apellido2: req.body.apellido2,
+//     edad: req.body.edad,
+//     correo: req.body.correo,
+//     telefono: req.body.telf,
+//   });
+
+// res.send("Gracias por rellenar el formulario :) ");
+// res.redirect("http://127.0.0.1:5500/index.html");
